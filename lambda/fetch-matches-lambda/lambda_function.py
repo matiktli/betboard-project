ff20d3a8f8844dc79db72034b9f412df31f67325
matiktli <m.kitlinos@gmail.com>
2020-07-18 22:54:57 +0100

    Work on fetch matches lambda

--- lambda/fetch-matches-lambda/lambda_function.py
+++ lambda/fetch-matches-lambda/lambda_function.py
@@ -1,9 +1,30 @@
 import logging
 import requests
 import boto3
+import botocore
+import os
+import json
+import time
 
-LOGGER = logging.getLogger()
-LOGGER.setLevel(logging.INFO)
+
+class Transformer():
+
+    def matchesToDbs(self, matches_dtos):
+        result = []
+        for match_dto in matches_dtos:
+            result.append(self.matchToDb(match_dto))
+        return result
+
+    def matchToDb(self, match_dto):
+        return {
+            'match_id': int(match_dto['id']),
+            'team_one_id': int(match_dto['teams']['home']['id']),
+            'team_two_id': int(match_dto['teams']['away']['id']),
+            'date': match_dto['time']['date'],
+            'leauge_id': int(match_dto['league']['id']),
+            'country_id': int(match_dto['league']['country_id']),
+            'json': match_dto
+        }
 
 
 class SoccerApi():
@@ -12,27 +33,41 @@ class SoccerApi():
     API_BASE_URL = 'https://api.soccersapi.com/v2.2'
     PATH_FIXTURES = API_BASE_URL + '/fixtures/'
 
-    def __init__(self, user, token):
+    DENMARK, SUPERLIGAEN = 37, 1609
+    AUSTRALIA, ALEAGUE = 14, 974
+
+    def __init__(self, user, token, transformer=Transformer()):
         self.user = user
         self.token = token
+        self.t = transformer
 
     def __add_auth(self, params):
         params['user'] = self.user
         params['token'] = self.token
 
+    def __check_response_valid(self, response):
+        response.raise_for_status()
+        if int(response.json()['meta']['count']) == 0:
+            return False
+        return True
+
     def find_matches_by_league_id_and_date(self, league_id, date):
-        params = {'country_id': league_id, 'd': date, 't': 'schedule'}
+        params = {'league_id': league_id, 'd': date, 't': 'schedule'}
         self.__add_auth(params)
         response = requests.get(SoccerApi.PATH_FIXTURES,
                                 params=params, headers=SoccerApi.HEADERS)
-        return response.json()['data']
+        if not self.__check_response_valid(response):
+            return []
+        return self.t.matchesToDbs(response.json()['data'])
 
     def find_match_by_id(self, match_id):
         params = {'id': match_id, 't': 'info'}
         self.__add_auth(params)
         response = requests.get(SoccerApi.PATH_FIXTURES,
                                 params=params, headers=SoccerApi.HEADERS)
-        return response.json()['data']
+        if not self.__check_response_valid(response):
+            return None
+        return self.t.matchToDb(response.json()['data'])
 
 
 class StoreSvc():
@@ -46,24 +81,51 @@ class StoreSvc():
     def __prefix_table(self, table_name):
         return self.env + '-' + table_name
 
-    def save_match(self, match):
+    def save_match_if_not_exists(self, match):
         table = self.dynamo.Table(self.__prefix_table(StoreSvc.MATCH_TABLE))
-        return table.put_item(Item=match)
+        try:
+            table.put_item(
+                Item=match,
+                ConditionExpression='attribute_not_exists(match_id)'
+            )
+        except botocore.exceptions.ClientError as ex:
+            if ex.response['Error']['Code'] != 'ConditionalCheckFailedException':
+                raise
+            else:
+                LOGGER.info("Match with id: {} already exists in db, skipping.".format(
+                    match['match_id']))
+        return True
+
+    def save_matches(self, matches) -> bool:
+        table = self.dynamo.Table(self.__prefix_table(StoreSvc.MATCH_TABLE))
+        with table.batch_writer() as batch:
+            for match in matches:
+                batch.put_item(Item=match)
+        return True
 
 
-class Transformer():
-
-    def matchToDb(self, match_dto):
-        return {
-            'match_id': int(match_dto['id']),
-            'team_one_id': int(match_dto['teams']['home']['id']),
-            'team_two_id': int(match_dto['teams']['away']['id']),
-            'date': match_dto['time']['date'],
-            'leauge_id': int(match_dto['league']['id']),
-            'country_id': int(match_dto['league']['country_id'])
-        }
+""" 
+    --- Actual Lambda Code --- 
+"""
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+SECRETS = json.loads(boto3.client('secretsmanager').get_secret_value(
+    SecretId='SoccerApiSecrets')['SecretString'])
+API = SoccerApi(SECRETS['SOCCER_API_USERNAME'],
+                SECRETS['SOCCER_API_TOKEN'])
+STORE = StoreSvc(os.getenv('ENV', 'prod'))
 
 
 def lambda_handler(event, context):
-    LOGGER.info('The event arrives hoho: {}'.format(event))
+    LOGGER.info('Consuming: {}'.format(event))
+    for job in event['data']['jobs']:
+        LOGGER.info('Executing job: {}'.format(job))
+        found_matches = API.find_matches_by_league_id_and_date(
+            job['params']['league_id'],
+            job['params']['date']
+        )
+        result = STORE.save_matches(found_matches)
+        job['status'] = 'done' if result else 'failed'
+        LOGGER.info('Job [{}]: {}'.format(job['status'], job))
+    LOGGER.info('Finished consuming: {}'.format(event))
     return True
