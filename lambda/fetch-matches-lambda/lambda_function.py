37daac86ca6fe5c82983d9031dc60e3fc777be7c
matiktli <m.kitlinos@gmail.com>
2020-07-18 17:50:11 +0100

    Working on saving

--- lambda/fetch-matches-lambda/lambda_function.py
+++ lambda/fetch-matches-lambda/lambda_function.py
@@ -1,6 +1,6 @@
 import logging
 import requests
-import boto
+import boto3
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
@@ -20,8 +20,8 @@ class SoccerApi():
         params['user'] = self.user
         params['token'] = self.token
 
-    def find_matches_by_leauge_id_and_date(self, leauge_id, date):
-        params = {'leauge_id': leauge_id, 'd': date, 't': 'schedule'}
+    def find_matches_by_league_id_and_date(self, league_id, date):
+        params = {'country_id': league_id, 'd': date, 't': 'schedule'}
         self.__add_auth(params)
         response = requests.get(SoccerApi.PATH_FIXTURES,
                                 params=params, headers=SoccerApi.HEADERS)
@@ -33,6 +33,35 @@ class SoccerApi():
         response = requests.get(SoccerApi.PATH_FIXTURES,
                                 params=params, headers=SoccerApi.HEADERS)
         return response.json()['data']
+
+
+class StoreSvc():
+
+    MATCH_TABLE = 'match'
+
+    def __init__(self, env):
+        self.env = env
+        self.dynamo = boto3.resource('dynamodb')
+
+    def __prefix_table(self, table_name):
+        return self.env + '-' + table_name
+
+    def save_match(self, match):
+        table = self.dynamo.Table(self.__prefix_table(StoreSvc.MATCH_TABLE))
+        return table.put_item(Item=match)
+
+
+class Transformer():
+
+    def matchToDb(self, match_dto):
+        return {
+            'match_id': int(match_dto['id']),
+            'team_one_id': int(match_dto['teams']['home']['id']),
+            'team_two_id': int(match_dto['teams']['away']['id']),
+            'date': match_dto['time']['date'],
+            'leauge_id': int(match_dto['league']['id']),
+            'country_id': int(match_dto['league']['country_id'])
+        }
 
 
 def lambda_handler(event, context):
